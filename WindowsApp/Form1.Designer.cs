3cd0316cb1f9ab5e4216784133e0debc10719a69
Anais Moreno <anabet.94@gmail.com>
2023-01-09 21:56:45 -0300

    Agregar archivos de proyecto.

---
+++ WindowsApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrearProducto = new System.Windows.Forms.Button();
+            this.btnCrearCliente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrearProducto
+            // 
+            this.btnCrearProducto.Location = new System.Drawing.Point(289, 109);
+            this.btnCrearProducto.Name = "btnCrearProducto";
+            this.btnCrearProducto.Size = new System.Drawing.Size(146, 23);
+            this.btnCrearProducto.TabIndex = 0;
+            this.btnCrearProducto.Text = "Crear Producto";
+            this.btnCrearProducto.UseVisualStyleBackColor = true;
+            this.btnCrearProducto.Click += new System.EventHandler(this.btnCrearProducto_Click);
+            // 
+            // btnCrearCliente
+            // 
+            this.btnCrearCliente.Location = new System.Drawing.Point(289, 224);
+            this.btnCrearCliente.Name = "btnCrearCliente";
+            this.btnCrearCliente.Size = new System.Drawing.Size(146, 23);
+            this.btnCrearCliente.TabIndex = 1;
+            this.btnCrearCliente.Text = "Crear Cliente";
+            this.btnCrearCliente.UseVisualStyleBackColor = true;
+            this.btnCrearCliente.Click += new System.EventHandler(this.btnCrearCliente_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCrearCliente);
+            this.Controls.Add(this.btnCrearProducto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrearProducto;
+        private System.Windows.Forms.Button btnCrearCliente;
+    }
+}
+
---
+++ WindowsApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsApp.Entidades;
+
+namespace WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrearProducto_Click(object sender, EventArgs e)
+        {
+            Producto sprite = new Producto() 
+            {
+                Nombre = "Sprite",
+                Descripcion= "Gaseosa sabor limon",
+                PrecioCosto=100,
+                Margen=0.3,
+                IVA = 0.21,
+                Proveedor= "Coca-Cola",
+                Categoria="Bebida",
+                SubCategoria="Gaseosa"
+            };
+
+            MessageBox.Show ("Producto Creado: "+sprite.Nombre+", Categoria: " +sprite.Categoria+", SubCategoria: "+sprite.SubCategoria+", Proveedor: "+sprite.Proveedor+", Precio de costo: "+sprite.PrecioCosto+", Margen de ganancia: "+(sprite.Margen*100)+"%, IVA: "+(sprite.IVA * 100)+"%, Precio Bruto: "+ Convert.ToDouble(sprite.PrecioBruto)+", Precio Venta: "+ Convert.ToDouble(sprite.PrecioVenta));
+
+
+
+
+        }
+
+        private void btnCrearCliente_Click(object sender, EventArgs e)
+        {
+            ClienteIndividuo Juan = new ClienteIndividuo() 
+            {
+                Nombre = "Juan Pablo",
+                Apellido = "Perez",
+                CUIT ="21-97035564-0",
+                Email= "juanpablo@hotmail.com",
+                Telefono = "11-56985467",
+                Direccion = "Av. Santa Fe, 954"
+            };
+
+            MessageBox.Show("Cliente Creado: " + Juan.Nombre + " " + Juan.Apellido + ", CUIT: "+Juan.CUIT+", E-mail: "+Juan.Email+", Telefono: "+Juan.Telefono+", Direccion:"+Juan.Direccion );
+
+
+        }
+    }
+}
